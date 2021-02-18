e841be398d4293802cd0f31576d2a3f68a338c36
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-02-18 16:37:50 -0300

    install test Jest - TDD

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -3,3 +3,19 @@ const postsData = require('../data/postsData');
 exports.getPosts = function(){
   return postsData.getPosts();
 };
+
+exports.getPosts = function (id){
+  return postsData.getPosts(id);
+};
+
+exports.savePost = function (post){
+  return postsData.savePost(post);
+};
+
+exports.deletePost = function (id){
+  return postsData.deletePost(id);
+};
+
+exports.updatePost = function (id,post){
+  return postsData.updatePost(id, post);
+};
